f526a601384648bfd7caf7a9e92df4905adc2684
Krista Blachian <KristaBlachian@outlook.com>
2018-08-03 01:29:16 -0400

    initial commit

---
+++ app/src/main/java/Adapter/MyAdapter.java
@@ -0,0 +1,44 @@
+package Adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+import Model.ListItem;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+    private Context context;
+    private List <ListItem> listItems;
+
+    public MyAdapter (Context context, List listitem) {
+
+    }
+
+
+    @NonNull
+    @Override
+    public MyAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
