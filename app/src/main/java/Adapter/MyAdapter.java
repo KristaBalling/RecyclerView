bfd5100fbcc37f586fc98b56d46972200f235e3b
Krista Blachian <KristaBlachian@outlook.com>
2018-08-07 01:11:55 -0400

    Displays correctly on emulator

--- app/src/main/java/Adapter/MyAdapter.java
+++ app/src/main/java/Adapter/MyAdapter.java
@@ -3,8 +3,12 @@ package Adapter;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.theironyard.recyclerview.recyclerview.R;
 
 import java.util.List;
 
@@ -15,30 +19,45 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
     private Context context;
     private List <ListItem> listItems;
 
-    public MyAdapter (Context context, List listitem) {
+    public MyAdapter (Context context, List listItem) {
+        this.context = context;
+        this.listItems = listItem;
 
     }
 
 
     @NonNull
     @Override
-    public MyAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_row, parent, false);
+
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyAdapter.ViewHolder holder, int position) {
 
+        ListItem item = listItems.get(position);
+        holder.name.setText(item.getName());
+        holder.description.setText(item.getDescription());
+
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return listItems.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
+        public TextView name;
+        public TextView description;
+
         public ViewHolder(View itemView) {
             super(itemView);
+
+            name = (TextView) itemView.findViewById(R.id.title);
+            description = (TextView) itemView.findViewById(R.id.description);
         }
     }
 }
