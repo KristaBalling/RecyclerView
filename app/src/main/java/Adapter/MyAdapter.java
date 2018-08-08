56588135f9a193c3b09f65d73d2e595e9553b9de
Krista Blachian <KristaBlachian@outlook.com>
2018-08-08 01:29:29 -0400

    added onClickListener, Toast

--- app/src/main/java/Adapter/MyAdapter.java
+++ app/src/main/java/Adapter/MyAdapter.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.theironyard.recyclerview.recyclerview.R;
 
@@ -51,21 +52,33 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
         return listItems.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         public TextView name;
         public TextView description;
         public TextView rating;
 
         public ViewHolder(View itemView, Context ctx, ArrayList<ListItem> items) {
             super(itemView);
+
+            itemView.setOnClickListener(this);
+
             listItems = items;
             context = ctx;
-
-
 
             name = (TextView) itemView.findViewById(R.id.title);
             description = (TextView) itemView.findViewById(R.id.description);
             rating = (TextView) itemView.findViewById(R.id.rating);
+        }
+
+        @Override
+        public void onClick(View v) {
+            //get position of the row clicked or tapped.
+            int position = getAdapterPosition();
+
+            ListItem item = listItems.get(position);
+
+            Toast.makeText(context, item.getName(), Toast.LENGTH_LONG).show();
+
         }
     }
 }
